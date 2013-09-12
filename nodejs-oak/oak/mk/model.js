e9de204b31796a5cd4ea31fe45370cc3f015ad17
afilimo <afilimo@LH7U0CNU313CHCY.ms.ds.uhc.com>
2013-09-12 18:59:25 -0500

    Model implemenation #2

--- nodejs-oak/oak/mk/model.js
+++ nodejs-oak/oak/mk/model.js
@@ -55,6 +55,35 @@ Id.prototype = {
 	}
 };
 
+exports.ChildNodeEntry = ChildNodeEntry = function(name, id) {
+
+	this.name = name;
+	ths.id = id;
+};
+
+ChildNodeEntry.prototype = {
+	getName : function() {
+		return this.name;
+	},
+
+	getId : function() {
+		return this.id;
+	},
+
+	equals : function(obj) {
+		if (this === obj) {
+			return true;
+		}
+		if (obj instanceof ChildNodeEntry) {
+			return (this.name == null ? obj.getName() == null : name.equals(obj
+					.getName())
+					&& this.id == null ? obj.getId() == null : id.equals(obj
+					.getId()));
+		}
+		return false;
+	}
+};
+
 AbstractCommit = function() {
 
 	// id of root node associated with this commit
@@ -199,15 +228,15 @@ AbstractNode.prototype = {
 		// @TODO forEach function in CildNodeEntires iterators
 		var otherEntries = node.getChildNodeEntries();
 
-		this.childEntries.getAdded().forEach(function(value, name) {
+		this.childEntries.getAdded(otherEntries, function(value, name) {
 			handler.childNodeAdded(value);
 		});
 
-		this.childEntries.getRemoved().forEach(function(value, name) {
+		this.childEntries.getRemoved(otherEntries, function(value, name) {
 			handler.childNodeDeleted(value);
 		});
 
-		this.childEntries.getModified().forEach(function(value, name) {
+		this.childEntries.getModified(otherEntries, function(value, name) {
 			modified = otherEntries.get(name);
 			handler.childNodeChanged(value, modified.getId());
 		});
@@ -231,6 +260,142 @@ AbstractNode.prototype = {
 			memory += childEntries.getMemory();
 		}
 
+		return memory;
+	}
+};
+
+exports.ChildNodeEntriesMap = ChildNodeEntriesMap = function(nodesMap) {
+
+	if (nodesMap == null) {
+		this.entries = new collections.HashMap();
+		return;
+	}
+
+	nodesMap.clone(this.entries);
+};
+
+ChildNodEntriesMap.prototype = {
+
+	equals : function(obj) {
+		if (obj instanceof ChildNodeEntriesMap) {
+			return this.entries.equals(obj.entries);
+		}
+		return false;
+	},
+
+	clone : function(entries) {
+		clone.entries = this.entries.clone();
+	},
+
+	inlined : function() {
+		return true;
+	},
+
+	getCount : function() {
+		return this.entries.count();
+	},
+
+	get : function(name) {
+		return this.entries.get(name);
+	},
+
+	getNames : function(offset, count, callback) {
+		this.getEntries(offset, count, callback);
+	},
+
+	getEntries : function(offset, count, callback) {
+		if (offset < 0 || count < -1) {
+			throw 'Illegal argument';
+		}
+
+		if (offset == 0 && count == -1) {
+			this.entries.forEach(callback);
+			return;
+		} else {
+			if (offset >= entries.size() || count == 0) {
+				return;
+			}
+			if (count == -1 || (offset + count) > entries.size()) {
+				count = entries.size() - offset;
+			}
+			this.entries.forEach(function(value, name) {
+				if (offset-- > 0)
+					return;
+				if (count-- > 0)
+					return;
+				callback(value, name);
+			});
+		}
+	},
+
+	add : function(entry) {
+		this.entries.set(entry.getName(), entry);
+	},
+
+	remove : function(name) {
+		return entries.remove(name);
+	},
+
+	rename : function(oldName, newName) {
+		if (oldName.equals(newName) || entries.get(oldName) == null)
+			return;
+		var clone = this.entries.clone();
+		this.entries.clear();
+		ChildNodeEntry
+		oldCNE = null;
+
+		clone.forEach(function(value, name) {
+			if (name.equals(oldName)) {
+				this.entries.set(newName, new ChildNodeEntry(newName, value
+						.getId()));
+			} else {
+				this.entries.set(name, value);
+			}
+		});
+	},
+
+	getAdded : function(other, callback) {
+		other.entries.forEach(function(value, name) {
+			if (!this.entries.has(name))
+				callback(value, name);
+		});
+	},
+
+	getRemoved : function(other, callback) {
+		this.entries.forEach(function(value, name) {
+			if (other.get(entry.getName()) == null)
+				callback(value, name);
+		});
+	},
+
+	getModified : function(other, callback) {
+		this.entries.forEach(function(value, name) {
+			var namesake = other.get(name);
+			if (namesake != null && !namesake.getId().equals(entry.getId()))
+				callback(value, name);
+		});
+	},
+
+	serialize : function(binding) {
+		// binding.writhMap accepts forEach iterator
+		binding.writeMap(":children", getCount(), this.entries.forEach);
+	},
+
+	deserialize : function(binding) {
+		var newInstance = new ChildNodeEntriesMap();
+		// Binding implement forEach iterator
+		binding.readBytesMap(":children", function(entry) {
+			newInstance.add(new ChildNodeEntry(entry.getKey(), new Id(entry
+					.getValue())));
+		});
+		return newInstance;
+	},
+
+	getMemory : function() {
+		var memory = 100;
+		this.entries.forEach(function(value, name) {
+			memory += name.length() * 2 + 100;
+		});
 		return memory;
 	}
 };
